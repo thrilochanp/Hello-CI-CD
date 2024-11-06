90ba50e6eaf0ad0e33b3c1cf23526dceab1416fa
thrilochanp <weblogic.xml@gmail.com>
2024-11-07 00:58:12 +0530

    simple hello ci-cd app

---
+++ my-simple-java-app/src/main/java/com/example/app/HelloCICDApplication.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCICDApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloCICDApplication.class, args);
+    }
+}
---
+++ my-simple-java-app/src/main/java/com/example/app/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, CI/CD!";
+    }
+}
